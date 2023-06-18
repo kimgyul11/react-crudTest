3790713e8f833347b8f910b1d34c48742a796509
kimgyul11 <coloo88@naver.com>
2023-06-18 20:54:16 +0900

    Reactmemo exam

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import { useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./components/DiaryEditor";
 import DiaryList from "./components/DiaryList";
-import Lifecycle from "./components/Lifecycle";
+import OptimizeTest from "./components/OptimizeTest";
 
 function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts").then(
+      (res) => res.json()
+    );
+    const initData = res.slice(0, 20).map((item) => {
+      return {
+        author: item.userId,
+        content: item.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        create_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (item) => {
     const { author, content, emotion } = item;
@@ -32,10 +52,24 @@ function App() {
       )
     );
   };
+
+  const getDiaryAnalysis = useMemo(() => {
+    const goodCount = data.filter((item) => item.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className="App">
-      <Lifecycle />
+      <OptimizeTest />
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio} %</div>
       <DiaryList dummyList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
   );
---
+++ src/components/OptimizeTest.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+
+const CounterA = React.memo(({ count }) => {
+  console.log("카운트 A업뎃");
+  return <div>{count}</div>;
+});
+
+const CounterB = ({ obj }) => {
+  console.log("카운트 B업뎃");
+  return <div>{obj.count}</div>;
+};
+
+const areEqual = (prevProps, nextProps) => {
+  if (prevProps.obj.count === nextProps.obj.count) {
+    return true;
+  }
+  return false;
+};
+const MemoziedCounterB = React.memo(CounterB, areEqual);
+
+export default function OptimizeTest() {
+  const [count, setCount] = useState(1);
+  const [obj, setObj] = useState({
+    count: 1,
+  });
+
+  return (
+    <div>
+      <div>
+        <h2>카운터 A</h2>
+        <CounterA count={count} />
+        <button onClick={() => setCount(count)}>A button</button>
+      </div>
+      <div>
+        <h2>카운터 B</h2>
+        <MemoziedCounterB obj={obj} />
+        <button onClick={() => setObj({ count: obj.count })}>B button</button>
+      </div>
+    </div>
+  );
+}
